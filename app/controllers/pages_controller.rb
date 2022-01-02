3c6b54b2c8aa1ea9536abb251617cd8edddcfa5d
laura person <lauraperson29800@gmail.com>
2022-01-02 22:53:31 +0100

    issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,9 +11,13 @@ class PagesController < ApplicationController
   def about
     unless Biography.nil?
       @biography = Biography.first.content
+    else
+      @biography = "Write your biography"
     end
     unless Event.nil?
       @events = Event.all.order(year: :DESC)
+    else
+      @events = "Enter an event"
     end
   end
 end
