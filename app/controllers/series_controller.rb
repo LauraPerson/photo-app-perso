af627cda8436bec4703ff9c348932246a9dcf390
laura person <lauraperson29800@gmail.com>
2022-01-07 11:13:29 +0100

    issue

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,5 +1,6 @@
 class SeriesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show] 
+
 
 
   def index
